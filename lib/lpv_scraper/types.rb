acde8240a471e7d83de5596986cf8d636aa7afec
Jan Paki <git@sternprodukt.de>
2022-11-26 19:46:42 +0100

    add custom types

--- lib/lpv_scraper/types.rb
+++ lib/lpv_scraper/types.rb
@@ -7,5 +7,11 @@ module LPVScraper
 
   module Types
     # Define your custom types here
+    ISO8601 = String.constrained(format: ISO8601_REGEXP)
+    ImportUrls = String.constrained(format: /^(?:(?<link>#{URI::DEFAULT_PARSER.make_regexp})\|\|)*\g<link>$|^$/)
+    HTML = Constructor(String) do |values|
+      doc = Nokogiri::HTML5.fragment(values, max_errors: -1)
+      doc.errors.any? ? nil : doc.to_s
+    end
   end
 end
